01af69cf2a0e536e2997aeb464050ca1ebed4358
Hornecker <peter@hornecker.net>
2017-04-10 00:21:24 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def processRequest(req):
     room = parameters.get("location-type")
     subject = parameters.get("subject-type")
 
-    url = "http://5.186.52.135:1000/webhook?state=" + state + "&room=" + room + "&subject=" + subject
+    url = "http://5.186.52.135:1000/webhook?state=\"" + state + "\"&room=\"" + room + "\"&subject=\"" + subject + "\""
     urlopen(url).read()
 
     speech = "Turning " + state + " " + subject + " now"
