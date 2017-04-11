bb15b94b4c2d2b91cdef3f53514c5dd15e2fbcb2
Hornecker <peter@hornecker.net>
2017-04-11 22:43:38 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -55,7 +55,11 @@ def processRequest(req):
     urlopen(url).read()
 
     # Building response to API.AI backend
-    speech = "Ok, light is " + state + " now"
+    if state = "on":
+        speech = "Ok, light on"
+    else
+        speech = "Ok, light off"
+
     data = ""
 
     print("Response:")
@@ -75,5 +79,4 @@ if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
 
     print("Starting app on port %d" % port)
-
     app.run(debug=False, port=port, host='0.0.0.0')
